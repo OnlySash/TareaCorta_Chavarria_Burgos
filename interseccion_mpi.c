60d8dce8915df0c2a818c1a8b6d5887e8570996c
OnlySash <schavar03@gmail.com>
2026-05-27 14:06:51 -0600

    Fase 1 funcional

--- interseccion_mpi.c
+++ interseccion_mpi.c
@@ -5,30 +5,52 @@
 #include <unistd.h>
 
 #define N_CARRILES 4
-#define N_VEHICULOS 50
+#define N_VEHICULOS 10
+#define MASTER 0
 
 #define TAG_SOLICITUD 1 /* carril → coordinador: pido cruzar */
 #define TAG_PERMISO 2 /* coordinador → carril: ya puede cruzar */
 #define TAG_CRUCE_FIN 3 /* carril → coordinador: terminé */
 #define TAG_ESTADISTICA 4 /* carril → coordinador: mis contadores */
 
-const char *nombres[N_CARRILES] = {"Coordinador","Norte", "Sur", "Este", "Oeste"};
+#define TAG_MENSAJE 5
+const char *nombres[N_CARRILES +1] = {"Coordinador","Norte", "Sur", "Este", "Oeste"};
 
-int vehiculos_cruzados = 0;
-int accidentes = 0;
-int en_cruce = 0;
-double tiempo = 0;
-int cola[N_CARRILES];
-int cruzados[N_CARRILES];
+typedef struct {
+    int carros_cruzados;
+    double tiempo_carril; 
+} mens_carril;
 
-void *hilo_carril(void *dato_del_carril) {
-  int numero_de_carril = *(int *)dato_del_carril;
-  unsigned int semilla = numero_de_carril + 1;
+void reporte_final(double tiempo_total , int carros_cruzados, int accidentes, mens_carril *carriles){
+    
+    printf("======== REPORTE FINAL ======== \n\n");
+    printf("Vehiculos cruzados: %d\n", carros_cruzados);
+    printf("Accidentes: %d\n", accidentes);
+    printf("Tiempo de simulacion: %.3f seg\n\n", tiempo_total);
+    printf("Estadisticas por carril: \n");
+    printf("Carril      Vehiculos   Espera acum. \n");
+    printf("Norte      %d  %f \n", carriles[1].carros_cruzados, carriles[1].tiempo_carril); 
+    printf("Sur:        %d  %f \n", carriles[2].carros_cruzados, carriles[2].tiempo_carril);
+    printf("Este        %d  %f \n", carriles[3].carros_cruzados, carriles[3].tiempo_carril);
+    printf("Oeste:      %d  %f \n\n", carriles[4].carros_cruzados, carriles[4].tiempo_carril);
+        
+    printf("Funciones MPI usadas: MPI_Send, MPI_Recv, MPI_Bcast, MPI_Barrier, MPI_Reduce, MPI_Gather");
+} 
 
+void hilo_carril(int rank, int n_carros) {
+  //unsigned int semilla = rank + time(NULL);
+
+  //Crear espacio para nombre y enviarlo
+  //char nombre_local[20];
+  //sprintf(nombre_local, "%s-%d", nombres[rank]);
+  MPI_Send((void*)nombres[rank], 20, MPI_CHAR, MASTER, TAG_MENSAJE, MPI_COMM_WORLD);
+  printf("[%s] listo. Esperando autorizacion para cruzar. \n", nombres[rank]);
+
+  /*int vehiculos_cruzados_local = 0;
+  int accidentes_local = 0;
+  int en_cruce_local = 0;
+  
   for (int i = 1; i <= N_VEHICULOS; i++) {
-    char nombre[20];
-    sprintf(nombre, "%s-%d", nombres[numero_de_carril], i);
-
     cola[numero_de_carril]++;
 
     // aqui es donde se da la Race condition en Fase 1
@@ -54,85 +76,74 @@ void *hilo_carril(void *dato_del_carril) {
     cruzados[numero_de_carril]++;
     //sem_post(&mutex_contadores);
   }
-  return NULL;
+  return NULL;*/
 }
 
-void reporte_final(){
-    double fin = MPI_Wtime();
-    printf("======== REPORTE FINAL ======== \n\n");
-    printf("Vehiculos cruzados: %d\n", vehiculos_cruzados);
-    printf("Accidentes: %d\n", accidentes);
-    printf("Tiempo de simulacion: %.3f seg\n\n", fin - inicio);
-    printf("Estadisticas por carril: \n")
-    printf("Carril      Vehiculos   Espera acum. \n")
-    printf("Norte      %d  %f \n", cruzados[0], cruzados[0]); 
-    printf("Sur:        %d  %f \n", cruzados[1], cruzados[1]);
-    printf("Este        %d  %f \n", cruzados[2] ,cruzados[2]);
-    printf("Oeste:      %d  %f \n\n", cruzados[3], cruzados[3]);
-        
-    printf("Funciones MPI usadas: MPI_Send, MPI_Recv, MPI_Bcast, MPI_Barrier, MPI_Reduce, MPI_Gather");
-} 
-
-void coordinador(){
-    if (size != N_CARRILES +1){
-    if (rank == 0)
-    {
-        prinf("Mayor cantidad de ranks");
-        MPI_Abort(MPI_COMM_WORLD, 1);
-    }
-    }
-  double inicio = MPI_Wtime();
-
+void coordinador(int n_carros, int rank){
+  double inicio = MPI_Wtime();  
+   
+  for (int i = 1; i <= N_CARRILES; i++)
+  {
+    char recibido[20];
+    MPI_Recv(recibido, 20, MPI_CHAR, i, TAG_MENSAJE, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+  }
   if (rank == MASTER) {
         //Inicializar desde rank 0
     }
-
-    MPI_Bcast(dna_chain, params.dna_length, MPI_CHAR, MASTER, MPI_COMM_WORLD);
    
-    MPI_Barrier(MPI_COMM_WORLD);
-
-    // process_data;
-    // Impresiones por rank
-
     //mpi_recv
     if (rank == MASTER) {
         //Recibir solicitudes de cruce
     }
 
-    //para impresiones locales, sincrinizar resultados
-    MPI_Gather(local_results, params.k_patterns * 2, MPI_INT, 
-               all_results,   params.k_patterns * 2, MPI_INT, 
-               MASTER, MPI_COMM_WORLD);
-
     if (rank == MASTER) {
         //en el rank 0
-        reporte_final();
+        double fin = MPI_Wtime();
+        //tiempo total
+        //reporte_final();
       }
 }
 
-void crear_mpi(){
+void crear_mpi(int argc, char *argv[]){
   //Fase 1, base
   int rank, size;
-  MPI_INit(&argc, &argc);
+  MPI_Init(&argc, &argv);
   //Nùmero de proceso/rank/ carril
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
   //Tamaño total del rank
   MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-  coordinador();
+  if (size != N_CARRILES + 1){
+    if (rank == MASTER)printf("ERROR en cantidad de procesos");
+    MPI_Abort(MPI_COMM_WORLD, 1);
+  }
+
+  int n_carros = N_VEHICULOS; 
+  if(rank == MASTER){
+    MPI_Bcast(&n_carros, 1, MPI_INT, MASTER, MPI_COMM_WORLD);
+    printf("[%s] Sistema iniciado, N_VEHICULOS= %d, distribuido a %d", nombres[0], N_VEHICULOS, N_CARRILES);
+  }else {
+    MPI_Bcast(&n_carros, 1, MPI_INT, MASTER, MPI_COMM_WORLD);
+  }
+  if(rank == MASTER){
+    coordinador(n_carros, rank);
+  } else{
+    hilo_carril(rank, N_VEHICULOS);
+  }
+  
 
   MPI_Finalize();
   //free variables
 }
 
-int main(void) {
+int main(int argc, char *argv[]) {
   printf("========================================================\n");
   printf("SIMULADOR DE INTERSECCION DE TRAFICO CON MPI\n");
   printf("========================================================\n");
-  printf("Procesos: %d | Carriles: %d | Vehiculos por carril: %d\n", ranks ,N_CARRILES, N_VEHICULOS);
+  printf("Procesos: %d | Carriles: %d | Vehiculos por carril: %d\n", N_CARRILES, N_CARRILES , N_VEHICULOS);
   printf("========================================================\n");
   
-  crear_mpi();
+  crear_mpi(argc, argv);
 
   return 0;
 }
